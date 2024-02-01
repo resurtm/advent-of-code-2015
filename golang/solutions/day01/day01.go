3b841ff2df2965b80fc48866343bf3768a047ad7
resurtm <resurtm@gmail.com>
2024-02-01 22:36:49 +0100

    Day 01, part 01, Golang.

--- golang/solutions/day01/day01.go
+++ golang/solutions/day01/day01.go
@@ -6,7 +6,15 @@ func SolveDay01() {
 	fmt.Println("Solve Day 01")
 }
 
-func solve(inp string) {
-	x := 0
-	fmt.Println(x)
+func solveInternal(inp string) int {
+	level := 0
+	for _, ch := range inp {
+		switch ch {
+		case '(':
+			level++
+		case ')':
+			level--
+		}
+	}
+	return level
 }
--- golang/solutions/day01/day01_test.go
+++ golang/solutions/day01/day01_test.go
@@ -1,1 +1,34 @@
 package day01_test
+
+import (
+	"aoc2015/solutions/day01"
+	"testing"
+)
+
+var testCases = []struct {
+	inp      string
+	expected int
+}{
+	{"(())", 0},
+	{"()()", 0},
+
+	{"(((", 3},
+	{"(()(()(", 3},
+
+	{"))(((((", 3},
+
+	{"())", -1},
+	{"))(", -1},
+
+	{")))", -3},
+	{")())())", -3},
+}
+
+func TestSolveInternal(t *testing.T) {
+	for _, tc := range testCases {
+		actual := day01.SolveInternal(tc.inp)
+		if actual != tc.expected {
+			t.Fatalf("inp='%s', expected='%d', actual='%d'", tc.inp, tc.expected, actual)
+		}
+	}
+}
