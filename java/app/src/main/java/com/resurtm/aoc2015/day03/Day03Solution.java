6911cde83db5b83ba93dd22c4c2e9dae954d15c3
resurtm <resurtm@gmail.com>
2024-02-05 21:00:05 +0100

    Day 03, part 2. Java.

--- java/app/src/main/java/com/resurtm/aoc2015/day03/Day03Solution.java
+++ java/app/src/main/java/com/resurtm/aoc2015/day03/Day03Solution.java
@@ -25,7 +25,9 @@ public class Day03Solution {
         lines.close();
 
         final int part1 = solvePart1(items.getFirst());
+        final int part2 = solvePart2(items.getFirst());
         System.out.printf("Day 02, part 1, %s: %d\n", kind, part1);
+        System.out.printf("Day 02, part 2, %s: %d\n", kind, part2);
     }
 
     int solvePart1(final String directions) {
@@ -33,9 +35,9 @@ public class Day03Solution {
         int posY = 0;
 
         final Set<String> visited = new HashSet<>();
-        visited.add(String.format("%d-%d", posX, posY));
 
         for (final Character direction : directions.toCharArray()) {
+            visited.add(String.format("%d-%d", posX, posY));
             switch (direction) {
                 case '^':
                     posY--;
@@ -50,9 +52,62 @@ public class Day03Solution {
                     posX++;
                     break;
             }
-            visited.add(String.format("%d-%d", posX, posY));
         }
 
+        visited.add(String.format("%d-%d", posX, posY));
+        return visited.size();
+    }
+
+    int solvePart2(final String directions) {
+        int pos1X = 0;
+        int pos1Y = 0;
+
+        int pos2X = 0;
+        int pos2Y = 0;
+
+        boolean robo = false;
+
+        final Set<String> visited = new HashSet<>();
+
+        for (final Character direction : directions.toCharArray()) {
+            if (robo) {
+                visited.add(String.format("%d-%d", pos1X, pos1Y));
+                switch (direction) {
+                    case '^':
+                        pos1Y--;
+                        break;
+                    case 'v':
+                        pos1Y++;
+                        break;
+                    case '<':
+                        pos1X--;
+                        break;
+                    case '>':
+                        pos1X++;
+                        break;
+                }
+            } else {
+                visited.add(String.format("%d-%d", pos2X, pos2Y));
+                switch (direction) {
+                    case '^':
+                        pos2Y--;
+                        break;
+                    case 'v':
+                        pos2Y++;
+                        break;
+                    case '<':
+                        pos2X--;
+                        break;
+                    case '>':
+                        pos2X++;
+                        break;
+                }
+            }
+            robo = !robo;
+        }
+
+        visited.add(String.format("%d-%d", pos1X, pos1Y));
+        visited.add(String.format("%d-%d", pos2X, pos2Y));
         return visited.size();
     }
 }
--- java/app/src/test/java/com/resurtm/aoc2015/day03/Day03SolutionTest.java
+++ java/app/src/test/java/com/resurtm/aoc2015/day03/Day03SolutionTest.java
@@ -9,17 +9,32 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class Day03SolutionTest {
     @ParameterizedTest
-    @MethodSource("provideTestSolveCases")
-    public void testSolve(final int expected, final String directions) {
+    @MethodSource("provideTestSolvePart1Cases")
+    public void testSolvePart1(final int expected, final String directions) {
         final int actual = (new Day03Solution()).solvePart1(directions);
         assertEquals(expected, actual);
     }
 
-    private static Stream<Arguments> provideTestSolveCases() {
+    private static Stream<Arguments> provideTestSolvePart1Cases() {
         return Stream.of(
                 Arguments.of(2, ">"),
                 Arguments.of(4, "^>v<"),
                 Arguments.of(2, "^v^v^v^v^v")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestSolvePart2Cases")
+    public void testSolvePart2(final int expected, final String directions) {
+        final int actual = (new Day03Solution()).solvePart2(directions);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideTestSolvePart2Cases() {
+        return Stream.of(
+                Arguments.of(3, "^v"),
+                Arguments.of(3, "^>v<"),
+                Arguments.of(11, "^v^v^v^v^v")
         );
     }
 }
