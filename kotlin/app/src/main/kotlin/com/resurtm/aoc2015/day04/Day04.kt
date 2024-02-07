cb3978a1d6ca5126fe9babfe43aeb8c838932d54
resurtm <resurtm@gmail.com>
2024-02-07 13:26:57 +0100

    Day 04, part 2. Kotlin implementation.

--- kotlin/app/src/main/kotlin/com/resurtm/aoc2015/day04/Day04.kt
+++ kotlin/app/src/main/kotlin/com/resurtm/aoc2015/day04/Day04.kt
@@ -12,10 +12,12 @@ fun solveDay04() {
 private fun runInternal(kind: String) {
     val lines = File("../../input/day04/$kind.txt").readLines()
     val part1 = solvePart1(lines.first())
+    val part2 = solvePart2(lines.first())
     println("Day 04, part 1, $kind: $part1")
+    println("Day 04, part 2, $kind: $part2")
 }
 
-internal fun solvePart1(secretKey: String): Int {
+internal fun solvePart1(secretKey: String, zeros: String = "00000"): Int {
     val md = MessageDigest.getInstance("MD5")
     var res = 0
     while (true) {
@@ -23,9 +25,11 @@ internal fun solvePart1(secretKey: String): Int {
         val hash = BigInteger(1, md.digest(hashVal.toByteArray()))
             .toString(16)
             .padStart(32, '0')
-        if (hash.startsWith("00000")) {
+        if (hash.startsWith(zeros)) {
             return res
         }
         res++
     }
 }
+
+internal fun solvePart2(secretKey: String): Int = solvePart1(secretKey, "000000")
